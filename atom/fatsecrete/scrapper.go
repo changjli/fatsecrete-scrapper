1dddf04735997a5f4e8661e4ecb219e0be8599fe
Nicholas Audric Surya <nicholasaudric123@gmail.com>
2024-09-29 21:31:08 +0700

    fix: change cholesterol to cholestrol

--- atom/fatsecrete/scrapper.go
+++ atom/fatsecrete/scrapper.go
@@ -90,210 +90,15 @@ func FatSecreteScrapper(foodName string) ([]Food, error) {
 
 	log.Printf("%d links found", len(links))
 
-	foods, err := FatSecreteScrapPage(driver, links)
-	if err != nil {
-		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-		return nil, err
+	var foods []Food
+	for _, link := range links {
+		food, err := FatSecreteScrapPage(driver, link)
+		if err != nil {
+			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+			return nil, err
+		}
+		foods = append(foods, food)
 	}
-
-	// var foods []Food
-	// for _, link := range links {
-	// 	var food Food
-
-	// 	err = driver.Get(link)
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	// prevent page not working
-	// 	time.Sleep(2 * time.Second)
-
-	// 	// brand
-	// 	brandElement, err := driver.FindElement(selenium.ByCSSSelector, ".manufacturer")
-	// 	if err != nil {
-	// 		food.Brand = "umum"
-	// 	} else {
-	// 		brand, err := brandElement.Text()
-	// 		if err != nil {
-	// 			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 			return nil, err
-	// 		}
-
-	// 		food.Brand = brand
-	// 	}
-
-	// 	// food
-	// 	foodNameElement, err := driver.FindElement(selenium.ByCSSSelector, "h1")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	foodName, err := foodNameElement.Text()
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	food.FoodName = foodName
-
-	// 	// portion
-	// 	portionElement, err := driver.FindElement(selenium.ByCSSSelector, ".serving_size_value")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	portion, err := portionElement.Text()
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	food.Portion = portion
-
-	// 	nutrientFactElement, err := driver.FindElement(selenium.ByCSSSelector, ".nutrition_facts")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	nutrientLabelElements, err := nutrientFactElement.FindElements(selenium.ByCSSSelector, ".nutrient.left")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	nutrientValueElements, err := nutrientFactElement.FindElements(selenium.ByCSSSelector, ".nutrient.right")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	// nutrients v2
-	// 	for i, nutrientLabelElement := range nutrientLabelElements {
-	// 		if i == 1 {
-	// 			continue
-	// 		}
-
-	// 		nutrientLabel, err := nutrientLabelElement.Text()
-	// 		if err != nil {
-	// 			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 			return nil, err
-	// 		}
-
-	// 		nutrientValue, err := nutrientValueElements[i+1].Text()
-	// 		if err != nil {
-	// 			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 			return nil, err
-	// 		}
-
-	// 		if nutrientLabel == "Energi" {
-	// 			food.Calorie = nutrientValue
-	// 		} else if nutrientLabel == "Lemak" {
-	// 			food.Fat = nutrientValue
-	// 		} else if nutrientLabel == "Kolestrol" {
-	// 			food.Cholestrol = nutrientValue
-	// 		} else if nutrientLabel == "Protein" {
-	// 			food.Protein = nutrientValue
-	// 		} else if nutrientLabel == "Karbohidrat" {
-	// 			food.Carbohydrate = nutrientValue
-	// 		} else if nutrientLabel == "Serat" {
-	// 			food.Fiber = nutrientValue
-	// 		} else if nutrientLabel == "Gula" {
-	// 			food.Sugar = nutrientValue
-	// 		} else if nutrientLabel == "Sodium" {
-	// 			food.Sodium = nutrientValue
-	// 		} else if nutrientLabel == "Kalium" {
-	// 			food.Kalium = nutrientValue
-	// 		}
-	// 	}
-
-	// 	// categories
-	// 	detailElement, err := driver.FindElement(selenium.ByCSSSelector, ".details")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	categories := ""
-	// 	h4Elements, err := detailElement.FindElements(selenium.ByCSSSelector, "h4")
-	// 	if err != nil {
-	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 		return nil, err
-	// 	}
-
-	// 	for _, h4Element := range h4Elements {
-	// 		h4, err := h4Element.Text()
-	// 		if err != nil {
-	// 			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 			return nil, err
-	// 		}
-
-	// 		log.Println(h4)
-
-	// 		prefix := "Jenis"
-
-	// 		if strings.HasPrefix(h4, prefix) {
-	// 			words := strings.Split(h4, " ")
-	// 			category := strings.ReplaceAll(words[len(words)-1], ":", "")
-	// 			if categories == "" {
-	// 				categories += category
-	// 			} else {
-	// 				categories += fmt.Sprintf(", %s", category)
-	// 			}
-	// 		}
-	// 	}
-
-	// 	food.Categories = categories
-
-	// 	// // nutrients
-	// 	// factElements, err := driver.FindElements(selenium.ByCSSSelector, ".fact")
-	// 	// if err != nil {
-	// 	// 	log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 	// 	return nil, err
-	// 	// }
-
-	// 	// for _, factElement := range factElements {
-
-	// 	// 	factTitleElement, err := factElement.FindElement(selenium.ByCSSSelector, ".factTitle")
-	// 	// 	if err != nil {
-	// 	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 	// 		return nil, err
-	// 	// 	}
-
-	// 	// 	factValueElement, err := factElement.FindElement(selenium.ByCSSSelector, ".factValue")
-	// 	// 	if err != nil {
-	// 	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 	// 		return nil, err
-	// 	// 	}
-
-	// 	// 	factTitle, err := factTitleElement.Text()
-	// 	// 	if err != nil {
-	// 	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 	// 		return nil, err
-	// 	// 	}
-
-	// 	// 	factValue, err := factValueElement.Text()
-	// 	// 	if err != nil {
-	// 	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-	// 	// 		return nil, err
-	// 	// 	}
-
-	// 	// 	if factTitle == "Kal" {
-	// 	// 		food.Calorie = factValue
-	// 	// 	} else if factTitle == "Lemak" {
-	// 	// 		food.Fat = factValue
-	// 	// 	} else if factTitle == "Karb" {
-	// 	// 		food.Carbohydrate = factValue
-	// 	// 	} else if factTitle == "Prot" {
-	// 	// 		food.Protein = factValue
-	// 	// 	}
-	// 	// }
-
-	// 	foods = append(foods, food)
-	// }
 
 	totalResponseTime := time.Since(timestart).Seconds()
 	log.Printf("Scrapping time: %f", totalResponseTime)
@@ -301,205 +106,198 @@ func FatSecreteScrapper(foodName string) ([]Food, error) {
 	return foods, nil
 }
 
-func FatSecreteScrapPage(driver selenium.WebDriver, links []string) ([]Food, error) {
-	var foods []Food
-	for _, link := range links {
-		var food Food
+func FatSecreteScrapPage(driver selenium.WebDriver, link string) (Food, error) {
+	var food Food
 
-		err := driver.Get(link)
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		// prevent page not working
-		time.Sleep(2 * time.Second)
-
-		// brand
-		brandElement, err := driver.FindElement(selenium.ByCSSSelector, ".manufacturer")
-		if err != nil {
-			food.Brand = "umum"
-		} else {
-			brand, err := brandElement.Text()
-			if err != nil {
-				log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-				return nil, err
-			}
-
-			food.Brand = brand
-		}
-
-		// food
-		foodNameElement, err := driver.FindElement(selenium.ByCSSSelector, "h1")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		foodName, err := foodNameElement.Text()
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		food.FoodName = foodName
-
-		// portion
-		portionElement, err := driver.FindElement(selenium.ByCSSSelector, ".serving_size_value")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		portion, err := portionElement.Text()
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		food.Portion = portion
-
-		nutrientFactElement, err := driver.FindElement(selenium.ByCSSSelector, ".nutrition_facts")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		nutrientLabelElements, err := nutrientFactElement.FindElements(selenium.ByCSSSelector, ".nutrient.left")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		nutrientValueElements, err := nutrientFactElement.FindElements(selenium.ByCSSSelector, ".nutrient.right")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		// nutrients v2
-		for i, nutrientLabelElement := range nutrientLabelElements {
-			if i == 1 {
-				continue
-			}
-
-			nutrientLabel, err := nutrientLabelElement.Text()
-			if err != nil {
-				log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-				return nil, err
-			}
-
-			nutrientValue, err := nutrientValueElements[i+1].Text()
-			if err != nil {
-				log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-				return nil, err
-			}
-
-			if nutrientLabel == "Energi" {
-				food.Calorie = nutrientValue
-			} else if nutrientLabel == "Lemak" {
-				food.Fat = nutrientValue
-			} else if nutrientLabel == "Kolestrol" {
-				food.Cholestrol = nutrientValue
-			} else if nutrientLabel == "Protein" {
-				food.Protein = nutrientValue
-			} else if nutrientLabel == "Karbohidrat" {
-				food.Carbohydrate = nutrientValue
-			} else if nutrientLabel == "Serat" {
-				food.Fiber = nutrientValue
-			} else if nutrientLabel == "Gula" {
-				food.Sugar = nutrientValue
-			} else if nutrientLabel == "Sodium" {
-				food.Sodium = nutrientValue
-			} else if nutrientLabel == "Kalium" {
-				food.Kalium = nutrientValue
-			}
-		}
-
-		// categories
-		detailElement, err := driver.FindElement(selenium.ByCSSSelector, ".details")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		categories := ""
-		h4Elements, err := detailElement.FindElements(selenium.ByCSSSelector, "h4")
-		if err != nil {
-			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-			return nil, err
-		}
-
-		for _, h4Element := range h4Elements {
-			h4, err := h4Element.Text()
-			if err != nil {
-				log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-				return nil, err
-			}
-
-			log.Println(h4)
-
-			prefix := "Jenis"
-
-			if strings.HasPrefix(h4, prefix) {
-				words := strings.Split(h4, " ")
-				category := strings.ReplaceAll(words[len(words)-1], ":", "")
-				if categories == "" {
-					categories += category
-				} else {
-					categories += fmt.Sprintf(", %s", category)
-				}
-			}
-		}
-
-		food.Categories = categories
-
-		// // nutrients
-		// factElements, err := driver.FindElements(selenium.ByCSSSelector, ".fact")
-		// if err != nil {
-		// 	log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-		// 	return nil, err
-		// }
-
-		// for _, factElement := range factElements {
-
-		// 	factTitleElement, err := factElement.FindElement(selenium.ByCSSSelector, ".factTitle")
-		// 	if err != nil {
-		// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-		// 		return nil, err
-		// 	}
-
-		// 	factValueElement, err := factElement.FindElement(selenium.ByCSSSelector, ".factValue")
-		// 	if err != nil {
-		// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-		// 		return nil, err
-		// 	}
-
-		// 	factTitle, err := factTitleElement.Text()
-		// 	if err != nil {
-		// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-		// 		return nil, err
-		// 	}
-
-		// 	factValue, err := factValueElement.Text()
-		// 	if err != nil {
-		// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
-		// 		return nil, err
-		// 	}
-
-		// 	if factTitle == "Kal" {
-		// 		food.Calorie = factValue
-		// 	} else if factTitle == "Lemak" {
-		// 		food.Fat = factValue
-		// 	} else if factTitle == "Karb" {
-		// 		food.Carbohydrate = factValue
-		// 	} else if factTitle == "Prot" {
-		// 		food.Protein = factValue
-		// 	}
-		// }
-
-		foods = append(foods, food)
+	err := driver.Get(link)
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
 	}
 
-	return foods, nil
+	// prevent page not working
+	time.Sleep(2 * time.Second)
+
+	// brand
+	brandElement, err := driver.FindElement(selenium.ByCSSSelector, ".manufacturer")
+	if err != nil {
+		food.Brand = "umum"
+	} else {
+		brand, err := brandElement.Text()
+		if err != nil {
+			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+			return Food{}, err
+		}
+
+		food.Brand = brand
+	}
+
+	// food
+	foodNameElement, err := driver.FindElement(selenium.ByCSSSelector, "h1")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	foodName, err := foodNameElement.Text()
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	food.FoodName = foodName
+
+	// portion
+	portionElement, err := driver.FindElement(selenium.ByCSSSelector, ".serving_size_value")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	portion, err := portionElement.Text()
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	food.Portion = portion
+
+	nutrientFactElement, err := driver.FindElement(selenium.ByCSSSelector, ".nutrition_facts")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	nutrientLabelElements, err := nutrientFactElement.FindElements(selenium.ByCSSSelector, ".nutrient.left")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	nutrientValueElements, err := nutrientFactElement.FindElements(selenium.ByCSSSelector, ".nutrient.right")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	// nutrients v2
+	for i, nutrientLabelElement := range nutrientLabelElements {
+		if i == 1 {
+			continue
+		}
+
+		nutrientLabel, err := nutrientLabelElement.Text()
+		if err != nil {
+			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+			return Food{}, err
+		}
+
+		nutrientValue, err := nutrientValueElements[i+1].Text()
+		if err != nil {
+			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+			return Food{}, err
+		}
+
+		if nutrientLabel == "Energi" {
+			food.Calorie = nutrientValue
+		} else if nutrientLabel == "Lemak" {
+			food.Fat = nutrientValue
+		} else if nutrientLabel == "Kolesterol" {
+			food.Cholestrol = nutrientValue
+		} else if nutrientLabel == "Protein" {
+			food.Protein = nutrientValue
+		} else if nutrientLabel == "Karbohidrat" {
+			food.Carbohydrate = nutrientValue
+		} else if nutrientLabel == "Serat" {
+			food.Fiber = nutrientValue
+		} else if nutrientLabel == "Gula" {
+			food.Sugar = nutrientValue
+		} else if nutrientLabel == "Sodium" {
+			food.Sodium = nutrientValue
+		} else if nutrientLabel == "Kalium" {
+			food.Kalium = nutrientValue
+		}
+	}
+
+	// categories
+	detailElement, err := driver.FindElement(selenium.ByCSSSelector, ".details")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	categories := ""
+	h4Elements, err := detailElement.FindElements(selenium.ByCSSSelector, "h4")
+	if err != nil {
+		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+		return Food{}, err
+	}
+
+	for _, h4Element := range h4Elements {
+		h4, err := h4Element.Text()
+		if err != nil {
+			log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+			return Food{}, err
+		}
+
+		prefix := "Jenis"
+
+		if strings.HasPrefix(h4, prefix) {
+			words := strings.Split(h4, " ")
+			category := strings.ReplaceAll(words[len(words)-1], ":", "")
+			if categories == "" {
+				categories += category
+			} else {
+				categories += fmt.Sprintf(", %s", category)
+			}
+		}
+	}
+
+	food.Categories = categories
+
+	// // nutrients
+	// factElements, err := driver.FindElements(selenium.ByCSSSelector, ".fact")
+	// if err != nil {
+	// 	log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+	// 	return nil, err
+	// }
+
+	// for _, factElement := range factElements {
+
+	// 	factTitleElement, err := factElement.FindElement(selenium.ByCSSSelector, ".factTitle")
+	// 	if err != nil {
+	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+	// 		return nil, err
+	// 	}
+
+	// 	factValueElement, err := factElement.FindElement(selenium.ByCSSSelector, ".factValue")
+	// 	if err != nil {
+	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+	// 		return nil, err
+	// 	}
+
+	// 	factTitle, err := factTitleElement.Text()
+	// 	if err != nil {
+	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+	// 		return nil, err
+	// 	}
+
+	// 	factValue, err := factValueElement.Text()
+	// 	if err != nil {
+	// 		log.Println("[fatsecrete][scrapper][FatSecreteScrapper]:", err)
+	// 		return nil, err
+	// 	}
+
+	// 	if factTitle == "Kal" {
+	// 		food.Calorie = factValue
+	// 	} else if factTitle == "Lemak" {
+	// 		food.Fat = factValue
+	// 	} else if factTitle == "Karb" {
+	// 		food.Carbohydrate = factValue
+	// 	} else if factTitle == "Prot" {
+	// 		food.Protein = factValue
+	// 	}
+	// }
+
+	return food, nil
 }
